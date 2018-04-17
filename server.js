6d0cc3a99dd96733edb686c71483f4599e229b46
neillsom <neillsom@mail.com>
2018-04-17 14:26:56 -0700

    added error handlers, logger middleware, and scratch/queries.js

--- server.js
+++ server.js
@@ -4,37 +4,65 @@ const express = require('express');
 
 const data = require('./db/notes');
 
+const { PORT } = require('./config');
+
+const { requestLogger } = require('./middleware/logger');
+
 const app = express();
+app.use(requestLogger);
 app.use(express.static('public'));
 
-// app.get('/api/notes', (req, res) => {
-//     res.json(data);
-// });
-
-app.get('/api/notes/:id', (req, res) => {
-    const id = req.params.id;
-    const item = data[id];
-
-    res.json(data.find(x => {
-        return x.id === Number(id);
-    }));
-});
-
 app.get('/api/notes', (req, res) => {
-    console.log('test!');
-    let results = data.slice();
-    if (req.query.searchTerm) {
-        console.log(req.query.searchTerm);
-        results = results.filter( item => item.title.toUpperCase().includes(req.query.searchTerm.toUpperCase()));
-        
+
+    const searchTerm = req.query.searchTerm;
+
+    if(searchTerm) {
+        let filteredList = data.filter(function(item) {
+            return item.title.includes(searchTerm);
+        });
+        res.json(filteredList);
+    } else {
+        res.json(data);
     }
-    res.json(results);
+ 
 });
+
+app.get('/boom', (req, res, next) => {
+    throw new Error('Boom!!');
+});
+
+
+app.get('/api/notes/:id', (req, res, next) => {
+    const id = +req.params.id;    
+    const foundItem = data.find(item => item.id === id);
+
+    if(foundItem) {
+        res.json(foundItem);
+    } else {
+        next();
+    }  
+});
+
+// error handler
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    res.status(404).json({ message: 'Not Found' });
+});
+
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.json({
+        message: err.message,
+        error: err
+    });
+});
+
 
 
 
 // Listen for incoming connections
-app.listen(8080, function () {
+app.listen(PORT, function () {
     console.info(`Server listening on ${this.address().port}`);
 }).on('error', err => {
     console.error(err);
