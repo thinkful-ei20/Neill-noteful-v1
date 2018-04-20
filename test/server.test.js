61171ce7419263e211f493d35379807a95e758c5
neillsom <neillsom@mail.com>
2018-04-20 13:39:38 -0700

    get search tests working

--- test/server.test.js
+++ test/server.test.js
@@ -45,3 +45,60 @@ describe('404 handler', function () {
             });
     });
 });
+
+describe('GET /api/notes', function () {
+
+    it('should return the default of 10 Notes ', function () {
+        return chai.request(app)
+            .get('/api/notes')
+            .then(function (res) {
+                expect(res).to.have.status(200);
+                expect(res).to.be.json;
+                expect(res.body).to.be.a('array');
+                expect(res.body).to.have.length(10);
+            });
+    });
+
+    it('should return a list with the correct fields', function () {
+        return chai.request(app)
+            .get('/api/notes')
+            .then(function (res) {
+                expect(res).to.have.status(200);
+                expect(res).to.be.json;
+                expect(res.body).to.be.a('array');
+                expect(res.body).to.have.length(10);
+
+                res.body.forEach(function (item) {
+                    expect(item).to.be.a('object');
+                    expect(item).to.include.keys('id', 'title', 'content');
+                });
+            });
+    });
+
+    it('should return correct search results for a valid query', function () {
+        return chai.request(app)
+            .get('/api/notes?searchTerm=millionaire')
+            .then(function (res) {
+                expect(res).to.have.status(200);
+                expect(res).to.be.json;
+                expect(res.body).to.be.a('array');
+                expect(res.body).to.have.length(1);
+                expect(res.body[0]).to.be.a('object');
+            });
+    });
+
+
+
+    it('should return an empty array for an incorrect query', function () {
+        return chai.request(app)
+            .get('/api/notes?searchTerm=Not%20a%20Valid%20Search')
+            .then(function (res) {
+                expect(res).to.have.status(200);
+                expect(res).to.be.json;
+                expect(res.body).to.be.a('array');
+                expect(res.body).to.have.length(0);
+            });
+    });
+
+});
+
